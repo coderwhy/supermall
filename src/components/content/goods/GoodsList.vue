291d1cdf269e4c3c27a3b31308c46f299e7123bb
Eloïse Schmitt <eloiseschmitt@iMac-de-Eloise.local>
2020-10-24 09:21:59 +0200

    add key attr to v-for

--- src/components/content/goods/GoodsList.vue
+++ src/components/content/goods/GoodsList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <goods-list-item v-for="item in goods" :goods-item="item"/>
+    <goods-list-item v-for="(item, id) in goods" :goods-item="item" :key="id"/>
   </div>
 </template>
 
